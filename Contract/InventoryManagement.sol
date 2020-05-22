822275e78fea643d29cf5f37bafc17b2dc235dc5
Jaime Alvarez <jaime_alv107@outlook.com>
2020-05-22 12:39:57 -0500

    updated InventoryManagement.sol with code that we went through class

--- Contract/InventoryManagement.sol
+++ Contract/InventoryManagement.sol
@@ -8,7 +8,7 @@ contract InventoryManagement is ERC721Full {
     constructor() ERC721Full("InventoryToken", "INV") public { }
     
     using Counters for Counters.Counter;
-    Counters.Counter token_ids;
+    Counters.Counter items_ids;
     Counters.Counter product_ids;
     Counters.Counter location_ids;
     Counters.Counter employee_ids;
@@ -19,28 +19,109 @@ contract InventoryManagement is ERC721Full {
     //     string ExpirationDate;
     // }
     
+    struct Item {
+        uint product_id;
+        string expiration_date;
+        // use require to verify if product is expired
+        bool is_active; // ??
+        uint location_id;
+    }
+    
+    // Do need product struct because is already created in web3
+    // struct Product {
+    //     string name;
+    //     // string description => mapping to URI
+    //     uint quantity;
+    // }
+    
+    //  location id is already created
+    // struct Location {
+    //     string name; // this can be in URI
+    //     string department;
+    // }
+    
+    
+    //
+    // struct Employee {
+    //     string name;
+    //     string deparment;
+    //     string role;
+    //     uint accessLevel;
+    // }
+    
     
     // mapping(uint => productDescription) public product_description;
-    mapping(uint => string) public Inventory;
-    mapping(uint => string) public Products;
+    mapping(uint => Item) public Inventory;
+    mapping(uint => string) public Products; // map to URI
     mapping(uint => string) public Locations;
     mapping(uint => string) public Employees;
     
-    event TransferToken(uint token_id, uint product_id, uint location_id, uint employee_id, string token_uri);
-    event ProductEvent();
+    
+    
+    event RegisterItem(uint employee_id, uint location_id, uint product_id);
+    event RegisterProduct(string name, string description, uint quantity)
+    event TransferItem(uint items_ids, uint product_id, uint location_id, uint employee_id, string item_uri);
     event LocationEvent();
     event EmployeeEvent();
     event InventoryUsedEvent();
     
-    function RegisterNewInventory(uint employee_id, uint location_id, uint product_id, string memory token_uri) public returns(uint) {
-        token_ids.increment();
-        
-        uint token_id = token_ids.current();
+    modifier is_active(item_id) {
+        require(Inventory[item_id].is_active);
+        _;
+    }
     
-        Inventory[token_id] = token_uri();
+    function registerNewItem(uint employee_id, uint location_id, uint product_id) public returns(uint) {
+        items_ids.increment();
         
-        return token_id;
+        uint item_id = item_ids.current();
+    
+        Inventory[item_id] = Item(product_id, expiration_date, is_active, location_id);
+    
+        emit RegisterItem(product_id, location_id, employee_id);
+        
+        return item_id;
+
     }  
+    
+    function registerNewProduct(string memory product_uri) public returns(uint) {
+        product_ids.increment();
+        
+        uint product_id = product_ids.current();
+        
+        Products[product_id] = product_id;
+        
+        emit RegisterProduct(product_uri)
+        
+        return product_id;
+    }
+    
+    
+    
+    function registerNewLocation(string name, string department) pu
+    
+    function registerNewEmployee
+    
+    
+    
+    
+    function itemLocationTransfer
+    
+    
+    function updateAccessLevel
+    
+    
+    
+    function updateItem
+    
+    function updateProduct
+    
+    function updateLocation
+    
+    function updateEmployee
+    
+    
+    
+    
     // Event to manage the function and a struct to reference it
     //Registering a new piece of inventory (crypto fax excercise 22.1 for routing URI to Pinata)
 
